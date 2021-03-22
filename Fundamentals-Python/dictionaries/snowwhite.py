ea203372359a179bce5d3887a005d6fcf2f83d83
Kalin Harmandzhiev <80389338+KalinHar@users.noreply.github.com>
2021-03-22 11:54:25 +0200

    Update snowwhite.py

--- Fundamentals-Python/dictionaries/snowwhite.py
+++ Fundamentals-Python/dictionaries/snowwhite.py
@@ -4,28 +4,20 @@ dwarfs = {}
 while command != 'Once upon a time':
     name, color, physics = command.split(' <:> ')
     if color not in dwarfs:
-        dwarfs[color] = {name: physics}
+        dwarfs[color] = {name: int(physics)}
     else:
         if name not in dwarfs[color]:
-            dwarfs[color].update({name: physics})
+            dwarfs[color].update({name: int(physics)})
         else:
-        dwarfs.update({name_colour : int(physics)})
+            if int(physics) > dwarfs[color][name]:
+                dwarfs[color][name] = int(physics)
     command = input()
 
-masive = []
-for dcn, ph in dwarfs.items():
-    co, na = dcn.split(' ')
-    masive.append([ph, co, na])
-new_masive = list(reversed(sorted(masive)))
-print(masive)
-print(new_masive)
+sort_dw_by_color = []
+for col, name_physics in sorted(dwarfs.items(), key=lambda x: -len(x[1])):
+    for dn in name_physics:
+        sort_dw_by_color.append([dn, col, name_physics[dn]])
 
-# Pesho <:> Red <:> 2000
-# Tosho <:> Blue <:> 1000
-# Gosho <:> Green <:> 1000
-# Tosho <:> Grey <:> 233
-# Tosho <:> Grey <:> 23
-# Tosho <:> Red <:> 23
-# Sasho <:> Yellow <:> 4500
-# Prakasho <:> Stamat <:> 1000
-# Once upon a time
+for dwarf in sorted(sort_dw_by_color, key=lambda x: -x[2]):
+    print(f"({dwarf[1]}) {dwarf[0]} <-> {dwarf[2]}")
+
